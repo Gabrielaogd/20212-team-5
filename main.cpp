a8501b78c902f21e085392deaac469f7bafb497f
Gabriela Ouriques <gabriela.o.dias@ufv.br>
2022-03-17 10:23:12 -0300

    Adição de peças inicialização e posicionamento no tabuleiro

--- main.cpp
+++ main.cpp
@@ -27,9 +27,59 @@ ALLEGRO_TIMER *timer = NULL;
 ALLEGRO_BITMAP *menu = NULL;
 ALLEGRO_BITMAP *mapa= NULL;
 ALLEGRO_BITMAP *mouse=NULL;
-ALLEGRO_BITMAP *dlaranja = NULL;
-ALLEGRO_BITMAP *droxo = NULL;
-ALLEGRO_BITMAP *dverde = NULL;
+
+//pecas
+ALLEGRO_BITMAP *docesimplesamarelo = NULL;
+ALLEGRO_BITMAP *docesimplesazul = NULL;
+ALLEGRO_BITMAP *docesimpleslaranja = NULL;
+ALLEGRO_BITMAP *docesimplesroxo = NULL;
+ALLEGRO_BITMAP *docesimplesverde = NULL;
+ALLEGRO_BITMAP *docesimplesvermelho = NULL;
+
+//bomba relogio
+ALLEGRO_BITMAP *bombarelogioamarelo = NULL;
+ALLEGRO_BITMAP *bombarelogioazul = NULL;
+ALLEGRO_BITMAP *bombarelogiolaranja = NULL;
+ALLEGRO_BITMAP *bombarelogioroxa = NULL;
+ALLEGRO_BITMAP *bombarelogioverde = NULL;
+ALLEGRO_BITMAP *bombarelogiovermelho = NULL;
+
+//especial horizontal
+ALLEGRO_BITMAP *especialhorizontalamarelo = NULL;
+ALLEGRO_BITMAP *especialhorizontalazul = NULL;
+ALLEGRO_BITMAP *especialhorizontallaranja = NULL;
+ALLEGRO_BITMAP *especialhorizontalroxo = NULL;
+ALLEGRO_BITMAP *especialhorizontalverde = NULL;
+ALLEGRO_BITMAP *especialhorizontalvermelho = NULL;
+
+//especial vertical
+ALLEGRO_BITMAP *especialverticalamarelo = NULL;
+ALLEGRO_BITMAP *especialverticalazul = NULL;
+ALLEGRO_BITMAP *especialverticallaranja = NULL;
+ALLEGRO_BITMAP *especialverticalroxo = NULL;
+ALLEGRO_BITMAP *especialverticalverde = NULL;
+ALLEGRO_BITMAP *especialverticalvermelho = NULL;
+
+//pacote fish
+ALLEGRO_BITMAP *pacotefishamarelo = NULL;
+ALLEGRO_BITMAP *pacotefishazul = NULL;
+ALLEGRO_BITMAP *pacotefishlaranja = NULL;
+ALLEGRO_BITMAP *pacotefishroxo = NULL;
+ALLEGRO_BITMAP *pacotefishverde = NULL;
+ALLEGRO_BITMAP *pacotefishvermelho = NULL;
+
+//peixe especial
+ALLEGRO_BITMAP *peixeespecialamarelo = NULL;
+ALLEGRO_BITMAP *peixeespecialazul = NULL;
+ALLEGRO_BITMAP *peixeespeciallaranja = NULL;
+ALLEGRO_BITMAP *peixeespecialroxo = NULL;
+ALLEGRO_BITMAP *peixeespecialverde = NULL;
+ALLEGRO_BITMAP *peixeespecialvermelho = NULL;
+
+//especiais
+ALLEGRO_BITMAP *rosquinharosaespecial = NULL;
+ALLEGRO_BITMAP *brigadeiroespecial= NULL;
+
 
 
 bool redraw = true;   
@@ -59,13 +109,13 @@ int k=0;
 
 //INICIALIZANDO O MAPA 
 int MAPA[8][8]={
+    0,1,2,3,0,1,1,2,
+    1,2,1,5,1,2,1,2,
+    2,5,0,1,0,2,3,0,
     0,1,2,0,0,1,1,2,
     1,2,1,0,1,2,1,2,
-    2,0,0,1,0,2,2,0,
-    0,1,2,0,0,1,1,2,
-    1,2,1,0,1,2,1,2,
-    2,0,0,1,0,2,2,0,
-    0,1,2,0,0,1,1,2,
+    2,0,0,1,6,2,5,0,
+    6,1,2,6,0,1,1,2,
     1,2,1,0,1,2,1,2,
 
 };
@@ -141,30 +191,335 @@ int inicializa() {
         
         return 0;
     }
-    dlaranja = al_load_bitmap("dlaranja.tga"); //carrega a imagem da cabeca
-	if(!dlaranja)
-    {
-        cout << "Falha ao carregar o doce laranja!" << endl;
+
+    //------------------------------------------ 
+
+    //Doces Simples
+
+    docesimplesamarelo = al_load_bitmap("docesimplesamarelo.bmp");
+    if(!docesimplesamarelo){
+        std::cout << "Falha ao carregar docesimplesamarelo!" << std::endl;
         al_destroy_display(display);
         return 0;
     }
 
+    docesimplesazul = al_load_bitmap("docesimpleszul.bmp");
+    if(!docesimplesazul){
+        std::cout << "Falha ao carregar docesimplesazul!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    docesimpleslaranja = al_load_bitmap("docesimpleslaranja.bmp");
+    if(!docesimpleslaranja){
+        std::cout << "Falha ao carregar docesimpleslaranja!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    docesimplesroxo = al_load_bitmap("docesimplesroxo.bmp");
+    if(!docesimplesroxo){
+        std::cout << "Falha ao carregar docesimplesroxo!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    docesimplesverde = al_load_bitmap("docesimplesverde.bmp");
+    if(!docesimplesverde){
+        std::cout << "Falha ao carregar docesimplesverde!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    docesimplesvermelho = al_load_bitmap("docesimplesvermelho.bmp");
+    if(!docesimplesvermelho){
+        std::cout << "Falha ao carregar docesimplesvermelho!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
     
-    droxo = al_load_bitmap("droxo.tga"); //carrega a imagem da cabeca
-	if(!droxo)
-    {
-        cout << "Falha ao carregar o doce roxo" << endl;
+    //-----------------------------------------
+
+    //Bombas Relogio
+
+    bombarelogioamarelo = al_load_bitmap("bombarelogioamarelo.bmp");
+    if(!bombarelogioamarelo){
+        std::cout << "Falha ao carregar a bomba relogio amarela!" << std::endl;
         al_destroy_display(display);
         return 0;
     }
 
-    dverde = al_load_bitmap("dverde.tga"); //carrega a imagem da cabeca
-	if(!droxo)
-    {
-        cout << "Falha ao carregar o doce verde" << endl;
+    bombarelogioazul = al_load_bitmap("bombarelogioazul.bmp");
+    if(!bombarelogioazul){
+        std::cout << "Falha ao carregar a bomba relogio azul!" << std::endl;
         al_destroy_display(display);
         return 0;
     }
+    
+    bombarelogiolaranja = al_load_bitmap("bombarelogiolaranja.bmp");
+    if(!bombarelogiolaranja){
+        std::cout << "Falha ao carregar a bomba relogio laranja!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    bombarelogioroxa = al_load_bitmap("bombarelogioroxa.bmp");
+    if(!bombarelogioroxa){
+        std::cout << "Falha ao carregar a bomba relogio roxa!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    bombarelogioverde = al_load_bitmap("bombarelogioverde.bmp");
+    if(!bombarelogioverde){
+        std::cout << "Falha ao carregar a bomba relogio verde!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    bombarelogiovermelho = al_load_bitmap("bombarelogiovermelho.bmp");
+    if(!bombarelogiovermelho){
+        std::cout << "Falha ao carregar a bomba relogio vermelho!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+    //----------------------------------------
+
+    //Especiais Horizontais 
+
+    especialhorizontalamarelo = al_load_bitmap("especialhorizontalamarelo.bmp");
+    if(!especialhorizontalamarelo){
+        std::cout << "Falha ao carregar especialhorizontalamarelo!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    especialhorizontalazul = al_load_bitmap("especialhorizontalazul.bmp");
+    if(!especialhorizontalazul){
+        std::cout << "Falha ao carregar especialhorizontalazul!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    especialhorizontallaranja = al_load_bitmap("especialhorizontallaranja.bmp");
+    if(!especialhorizontallaranja){
+        std::cout << "Falha ao carregar especialhorizontallaranja!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    especialhorizontalroxo = al_load_bitmap("especialhorizontalroxo.bmp");
+    if(!especialhorizontalroxo){
+        std::cout << "Falha ao carregar especialhorizontalroxo!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    especialhorizontalverde = al_load_bitmap("especialhorizontalverde.bmp");
+    if(!especialhorizontalverde){
+        std::cout << "Falha ao carregar especialhorizontalverde!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    especialhorizontalvermelho = al_load_bitmap("especialhorizontalvermelho.bmp");
+    if(!especialhorizontalvermelho){
+        std::cout << "Falha ao carregar especialhorizontalvermelho!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+    //----------------------------------------
+
+     //Especiais Verticais 
+
+    especialverticalamarelo = al_load_bitmap("especialverticalamarelo.bmp");
+    if(!especialverticalamarelo){
+        std::cout << "Falha ao carregar especialverticalamarelo!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    especialverticalazul = al_load_bitmap("especialverticalazul.bmp");
+    if(!especialverticalazul){
+        std::cout << "Falha ao carregar especialverticalazul!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    especialverticallaranja = al_load_bitmap("especialverticallaranja.bmp");
+    if(!especialverticallaranja){
+        std::cout << "Falha ao carregar especialverticallaranja!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    especialverticalroxo = al_load_bitmap("especialverticalroxo.bmp");
+    if(!especialverticalroxo){
+        std::cout << "Falha ao carregar especialverticalroxo!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    especialverticalverde = al_load_bitmap("especialverticalverde.bmp");
+    if(!especialverticalverde){
+        std::cout << "Falha ao carregar especialverticalverde!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    especialverticalvermelho = al_load_bitmap("especialverticalvermelho.bmp");
+    if(!especialverticalvermelho){
+        std::cout << "Falha ao carregar especialverticalvermelho!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+    //----------------------------------------
+
+    //Pacote Fish
+
+    pacotefishamarelo = al_load_bitmap("pacotefishamarelo.bmp");
+    if(!pacotefishamarelo){
+        std::cout << "Falha ao carregar pacotefishamarelo!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    pacotefishazul = al_load_bitmap("pacotefishazul.bmp");
+    if(!pacotefishazul){
+        std::cout << "Falha ao carregar pacotefishazul!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    pacotefishlaranja = al_load_bitmap("pacotefishlaranja.bmp");
+    if(!pacotefishlaranja){
+        std::cout << "Falha ao carregar pacotefishlaranja!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    pacotefishroxo = al_load_bitmap("pacotefishroxo.bmp");
+    if(!pacotefishroxo){
+        std::cout << "Falha ao carregar pacotefishroxo!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    pacotefishverde = al_load_bitmap("pacotefishverde.bmp");
+    if(!pacotefishverde){
+        std::cout << "Falha ao carregar pacotefishverde!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    pacotefishvermelho = al_load_bitmap("pacotefishvermelho.bmp");
+    if(!pacotefishvermelho){
+        std::cout << "Falha ao carregar pacotefishvermelho!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    //----------------------------------------
+
+    //Peixe Especial
+
+    peixeespecialamarelo = al_load_bitmap("peixeespecialamarelo.bmp");
+    if(!peixeespecialamarelo){
+        std::cout << "Falha ao carregar peixeespecialamarelo!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    peixeespecialazul = al_load_bitmap("peixeespecialazul.bmp");
+    if(!peixeespecialazul){
+        std::cout << "Falha ao carregar peixeespecialazul!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    peixeespeciallaranja = al_load_bitmap("peixeespeciallaranja.bmp");
+    if(!peixeespeciallaranja){
+        std::cout << "Falha ao carregar peixeespeciallaranja!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    peixeespecialroxo = al_load_bitmap("peixeespecialroxo.bmp");
+    if(!peixeespecialroxo){
+        std::cout << "Falha ao carregar peixeespecialroxo!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    peixeespecialverde = al_load_bitmap("peixeespecialverde.bmp");
+    if(!peixeespecialverde){
+        std::cout << "Falha ao carregar peixeespecialverde!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    peixeespecialvermelho = al_load_bitmap("peixeespecialvermelho.bmp");
+    if(!peixeespecialvermelho){
+        std::cout << "Falha ao carregar peixeespecialvermelho!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    //----------------------------------------
+
+    //Especiais
+
+    rosquinharosaespecial = al_load_bitmap("rosquinharosaespecial.bmp");
+    if(!peixeespecialvermelho){
+        std::cout << "Falha ao carregar rosquinharosaespecial!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    brigadeiroespecial = al_load_bitmap("brigadeiroespecial.bmp");
+    if(!brigadeiroespecial){
+        std::cout << "Falha ao carregar brigadeiroespecial!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+
+    //al_init_font_addon();  // marcado para possibilitar a execução
+    //al_init_ttf_addon();  //  marcado para possibilitar a execução
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+    al_register_event_source(event_queue, al_get_mouse_event_source());
+
+    al_clear_to_color(al_map_rgb(0,0,0));
+    al_flip_display();
+    al_start_timer(timer);
+
+    return 1;
+}
+
+
+int main(int argc, char **argv){
+    
+    if(!inicializa()) return -1;
+    while(!sair){
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(event_queue, &ev);
+         if(ev.type == ALLEGRO_EVENT_TIMER)
+        {
+        
+            redraw = true;
+        }
+        else if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
+        {
+            switch(ev.keyboard.keycode)
+            {
+            
+            }
+        }
+	    //<------------------------------------------------------->
+
     //al_init_font_addon();  // marcado para possibilitar a execução
     //al_init_ttf_addon();  //  marcado para possibilitar a execução
     al_register_event_source(event_queue, al_get_display_event_source(display));
@@ -285,17 +640,29 @@ int main(int argc, char **argv){
             for (int i=0; i<8; i++){
                 for (int j=0; j<8; j++){
                      int auxteste;
-                    if(MAPA[i][j]==0){ //Se for 0, desenha a peça laranha
-                        al_draw_bitmap(dlaranja,j*q,i*q,0);
-                        
+                     
+                    if(MAPA[i][j]==0){ //Se for 0, desenha a peça laranja
+                        al_draw_bitmap(docesimpleslaranja,j*q,i*q,0);
                     }
                     if(MAPA[i][j]==1){ //Se for 1, desenha a peça roxa;
-                       al_draw_bitmap(droxo,j*q,i*q,0); //função desenha
+                        al_draw_bitmap(docesimplesroxo,j*q,i*q,0); //função desenha
                     }
-                    
-
+                        
                     if(MAPA[i][j]==2){//Se for 2, desenha a peça verde;
-                        al_draw_bitmap(dverde,j*q,i*q,0); //função desenha
+                        al_draw_bitmap(docesimplesverde,j*q,i*q,0); //função desenha
+                    }
+
+                    if(MAPA[i][j]==3){ //Se for 3, desenha a peça vermelha
+                        al_draw_bitmap(docesimpleslaranja,j*q,i*q,0);
+                    }
+
+                    if(MAPA[i][j]==5){ //Se for 5, desenha a peça amarela;
+                        al_draw_bitmap(docesimplesroxo,j*q,i*q,0); //função desenha
+                    }
+                        
+
+                    if(MAPA[i][j]==6){//Se for 6, desenha a peça azul;
+                        al_draw_bitmap(docesimplesverde,j*q,i*q,0); //função desenha
                     }
 
                      ///EFEITO GRAVIDADE DAS PEÇAS
@@ -331,14 +698,61 @@ int main(int argc, char **argv){
         al_draw_bitmap(menu, 0,0,0);
 		al_flip_display();
 
-	
-
-    al_destroy_bitmap(dlaranja);
-    al_destroy_bitmap(droxo);
-    al_destroy_bitmap(dverde);
     al_destroy_timer(timer);
     al_destroy_display(display);
     al_destroy_event_queue(event_queue);
+
+    //pecas
+    al_destroy_bitmap(docesimplesamarelo);
+    al_destroy_bitmap(docesimplesazul);
+    al_destroy_bitmap(docesimpleslaranja);
+    al_destroy_bitmap(docesimplesroxo);
+    al_destroy_bitmap(docesimplesverde);
+    al_destroy_bitmap(docesimplesvermelho);
+
+    //bomba relogio
+    al_destroy_bitmap(bombarelogioamarelo);
+    al_destroy_bitmap(bombarelogioazul);
+    al_destroy_bitmap(bombarelogiolaranja);
+    al_destroy_bitmap(bombarelogioroxa);
+    al_destroy_bitmap(bombarelogioverde);
+    al_destroy_bitmap(bombarelogiovermelho);
+
+    //especial horizontal
+    al_destroy_bitmap(especialhorizontalamarelo);
+    al_destroy_bitmap(especialhorizontalazul);
+    al_destroy_bitmap(especialhorizontallaranja);
+    al_destroy_bitmap(especialhorizontalroxo);
+    al_destroy_bitmap(especialhorizontalverde);
+    al_destroy_bitmap(especialhorizontalvermelho);
+
+    //especial vertical
+    al_destroy_bitmap(especialverticalamarelo);
+    al_destroy_bitmap(especialverticalazul);
+    al_destroy_bitmap(especialverticallaranja);
+    al_destroy_bitmap(especialverticalroxo);
+    al_destroy_bitmap(especialverticalverde);
+    al_destroy_bitmap(especialverticalvermelho);
+
+    //pacote fish
+    al_destroy_bitmap(pacotefishamarelo);
+    al_destroy_bitmap(pacotefishazul);
+    al_destroy_bitmap(pacotefishlaranja);
+    al_destroy_bitmap(pacotefishroxo);
+    al_destroy_bitmap(pacotefishverde);
+    al_destroy_bitmap(pacotefishvermelho);
+
+    //peixe especial
+    al_destroy_bitmap(peixeespecialamarelo);
+    al_destroy_bitmap(peixeespecialazul);
+    al_destroy_bitmap(peixeespeciallaranja);
+    al_destroy_bitmap(peixeespecialroxo);
+    al_destroy_bitmap(peixeespecialverde);
+    al_destroy_bitmap(peixeespecialvermelho);
+
+    //especiais
+    al_destroy_bitmap(rosquinharosaespecial);
+    al_destroy_bitmap(brigadeiroespecial);
     
     return 0;
 }
